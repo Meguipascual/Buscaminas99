82286aaed3118b53a1b03673ff78a094abb4b802
Meguipascual <meguipascual@gmail.com>
2022-02-23 20:15:30 +0100

    Imported Hazel

---
+++ buscaminas99/Assets/Scripts/Libraries/Hazel/NewConnectionEventArgs.cs
@@ -0,0 +1,22 @@
+﻿namespace Hazel
+{
+    public struct NewConnectionEventArgs
+    {
+        /// <summary>
+        /// The data received from the client in the handshake.
+        /// You must not recycle this. If you need the message outside of a callback, you should copy it.
+        /// </summary>
+        public readonly MessageReader HandshakeData;
+
+        /// <summary>
+        /// The <see cref="Connection"/> to the new client.
+        /// </summary>
+        public readonly Connection Connection;
+
+        public NewConnectionEventArgs(MessageReader handshakeData, Connection connection)
+        {
+            this.HandshakeData = handshakeData;
+            this.Connection = connection;
+        }
+    }
+}
