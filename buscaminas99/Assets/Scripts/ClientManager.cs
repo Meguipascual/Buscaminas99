0a06bef55b5ce35e293aa4317519f92c9ed7b39e
Meguipascual <meguipascual@gmail.com>
2022-03-23 22:28:53 +0100

    Fix unhandled client connect async

--- buscaminas99/Assets/Scripts/ClientManager.cs
+++ buscaminas99/Assets/Scripts/ClientManager.cs
@@ -12,21 +12,41 @@ public class ClientManager : MonoBehaviour
 {
     private UnityUdpClientConnection clientConnection;
     private Queue<int> cellIdsToProcess = new Queue<int>();
+    private Queue<NetworkMessage> unsentMessages = new Queue<NetworkMessage>();
     private BoardManager rivalBoardManager;
     private bool seedMessageProccessed;
     private int messagesProcessedCount;
-    // Start is called before the first frame update
+
     void Start()
     {
         string serverIp = "78.45.45.231";
-        // IPAddress.Loopback volver a ponerlo en vez de lo de alberto para futuras pruebas locales de server
+        // IPAddress.Parse(serverIp) volver a ponerlo en vez de lo de alberto para futuras pruebas locales de server
         FindObjectOfType<NetworkManager>().IsClient = true;
         rivalBoardManager = GameObject.FindGameObjectWithTag(Tags.RivalBoard).GetComponent<BoardManager>();
-        clientConnection = new UnityUdpClientConnection(new UnityLogger(true), new IPEndPoint(IPAddress.Parse(serverIp), 6501));
+        clientConnection = new UnityUdpClientConnection(new UnityLogger(true), new IPEndPoint(IPAddress.Loopback, 6501));
         clientConnection.DataReceived += HandleMessage;
         clientConnection.ConnectAsync();
     }
+
     private void Update()
+    {
+        SendPendingMessages();
+        ProcessRivalCellIds();
+    }
+
+    private void SendPendingMessages()
+    {
+        if (clientConnection.State == ConnectionState.Connected)
+        {
+            while (unsentMessages.Count > 0)
+            {
+                var message = unsentMessages.Dequeue();
+                clientConnection.SendBytes(NetworkUtils.ObjectToByteArray(message));
+            }
+        }
+    }
+
+    private void ProcessRivalCellIds()
     {
         while (cellIdsToProcess.Count > 0)
         {
@@ -41,14 +61,25 @@ public class ClientManager : MonoBehaviour
             cell.UseCell();
         }
         cellIdsToProcess.Clear();
-
     }
-    public new void SendMessage(string msg, int cellId)
+
+    public void SendMessage(string msg, int cellId)
     {
+        if(clientConnection.State != ConnectionState.Connected)
+        {
+            unsentMessages.Enqueue(new NetworkMessage() { Text = msg, CellId = cellId });
+            return;
+        }
         clientConnection.SendBytes(NetworkUtils.ObjectToByteArray (new NetworkMessage() { Text = msg, CellId = cellId}));
     }
-    public new void SendSeedMessage(string msg, int seed)
+
+    public void SendSeedMessage(string msg, int seed)
     {
+        if (clientConnection.State != ConnectionState.Connected)
+        {
+            unsentMessages.Enqueue(new NetworkMessage() { Text = msg, Seed = seed });
+            return;
+        }
         clientConnection.SendBytes(NetworkUtils.ObjectToByteArray(new NetworkMessage() { Text = msg, Seed = seed }));
     }
 
